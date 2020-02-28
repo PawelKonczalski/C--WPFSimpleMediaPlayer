1fdc89e25e0ff34dc7d91441544efe41ed670929
PawelKonczalski <50076223+PawelKonczalski@users.noreply.github.com>
2020-02-28 12:48:21 +0100

    statusbar, progressbar and multivalueconverter

---
+++ wpfMediaPlayer/TwoValuesPercentageConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace wpfMediaPlayer
+{
+    class TwoValuesPercentageConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            double watchedTime = (double)values[0];
+            double totalTime = (double)values[1];
+            return watchedTime / totalTime;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
